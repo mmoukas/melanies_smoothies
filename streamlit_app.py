8cc853d4abd003a70657a90c1f301833d8247ad9
mmoukas <michalismoukas@eaton.com>
2024-06-05 19:01:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-
 cnx=st.connection("snowflake")
 session=cnx.session()
 
